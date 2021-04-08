5911053badcca2ec08a155af2a9b784ffe88591a
Vinicius Motta <viniciusmotta8@gmail.com>
2021-04-08 11:50:48 -0300

    feat: update wordsidf

--- src/handlers/services/translateService.ts
+++ src/handlers/services/translateService.ts
@@ -6,18 +6,18 @@ export class TranslateService {
   async execute (url: string) {
     const formattedUrl = url.split('%2F').join('/')
     try {
-      const words = (await axios.get('https://raw.githubusercontent.com/vmotta8/translate-ms/master/dataset/ww.json')).data
       const subtitle = (await axios.get(formattedUrl)).data
       const subtitleWordsArray = functions.stringToArray(subtitle, 3)
       const subtitleWordsCounted = functions.countWords(subtitleWordsArray)
 
       const subtitleTF = functions.tf(subtitleWordsCounted)
-      const wordsIDF = functions.idf(words.index, words.numberOfDocs)
-      const subtitleTFIDF = functions.tfidf(subtitleTF, wordsIDF, words)
+      const wordsIDF = (await axios.get('https://raw.githubusercontent.com/vmotta8/translate-ms/master/dataset/wordsIDF.json')).data
+      const subtitleTFIDF = functions.tfidf(subtitleTF, wordsIDF)
       const sortedWordsTFIDF = functions.sortable(subtitleTFIDF, 100)
 
       const translatedWords = await functions.translateArrayOfWords(sortedWordsTFIDF, 'en', 'pt')
       const newSubtitle = functions.addTranslatedWordsToSubtitle(subtitle, translatedWords)
+
       return newSubtitle
     } catch (err) {
       console.log(err)
