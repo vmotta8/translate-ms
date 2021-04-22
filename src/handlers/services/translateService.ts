d700d7da587af255b6003d45d8386f43a9525b7f
Vinicius Motta <viniciusmotta8@gmail.com>
2021-04-22 01:59:00 -0300

    refactor: optimized service

--- src/handlers/services/translateService.ts
+++ src/handlers/services/translateService.ts
@@ -10,29 +10,29 @@ export class TranslateService {
   ) {}
 
   async execute (url: string) {
-    const formattedUrl = url.split('%2F').join('/')
-
     this.cache.connect()
     try {
       let newSubtitle
-      const cacheEntry = await this.cache.get(`${url}`)
+      const cacheEntry = await this.cache.get(url)
 
       if (cacheEntry) {
         newSubtitle = cacheEntry
       } else {
+        const formattedUrl = url.split('%2F').join('/')
         const subtitle = (await axios.get(formattedUrl)).data
+
         const subtitleWordsArray = functions.stringToArray(subtitle, 3)
         const subtitleWordsCounted = functions.countWords(subtitleWordsArray)
 
         const subtitleTF = functions.tf(subtitleWordsCounted)
-        const wordsIDF = (await axios.get('https://raw.githubusercontent.com/vmotta8/translate-ms/master/dataset/wordsIDF.json')).data
+        const wordsIDF = (await axios.get('https://raw.githubusercontent.com/vmotta8/translate-ms/master/dataset/words.json')).data
         const subtitleTFIDF = functions.tfidf(subtitleTF, wordsIDF)
-        const sortedWordsTFIDF = functions.sortable(subtitleTFIDF, 100)
+        const sortedWordsTFIDF = functions.sortable(subtitleTFIDF, 200)
 
         const translatedWords = await functions.translateArrayOfWords(sortedWordsTFIDF, 'en', 'pt')
         newSubtitle = functions.addTranslatedWordsToSubtitle(subtitle, translatedWords)
 
-        await this.cache.set(`${url}`, newSubtitle, 15)
+        await this.cache.set(url, newSubtitle, 15)
       }
       this.cache.disconnect()
 
